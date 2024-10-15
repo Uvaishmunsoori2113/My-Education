18148d037e12f71dcc7dc89805bbd158f53d29aa
Uvaishmunsoori2113 <140684085+Uvaishmunsoori2113@users.noreply.github.com>
2024-10-16 00:23:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $(".burger").click(function () {
+        $(".nav-list").slideToggle();
+        // $(".nav-list").slideUp(11000);
+        // $(".nav-list").slideDown(11000);
+    });
+});
+
